9e3936cf1384068e719879c0d0bed9693942b2b6
Spill-Tea <61038372+Spill-Tea@users.noreply.github.com>
2024-03-28 16:30:29 -0700

    Initial project setup + additions to docs setup
    * Simple file structure setup.
    
    * Pyproject toml configuration.
    
    * Include a requirements txt.
    
    * Simple read the docs config file.
    
    * Update pyproject.toml. Use ruff linter and update configuration.
    
    * Tox configuration file.
    
    * Update Project Name.
    
    * Update Project Name in documentation. Include a few more sphinx extensions.
    
    * Seperate source documentation files.
    
    * Update source directory for documentation.
    
    * Define sphinx version for doc build.
    
    * Add Project path to work with extensions. Temporarily comment out linkcode until we provide error handling function.

--- docs/conf.py
+++ docs/conf.py
@@ -6,14 +6,26 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = "new-compiler"
-copyright = "2024, Christopher Priebe, Jason Delrio"
-author = "Christopher Priebe, Jason Delrio"
+project = "FhY"
+copyright = "2024, Christopher Priebe, Jason Del Rio"
+author = "Christopher Priebe, Jason Del Rio"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+import os
+import sys
 
-extensions = []
+# https://sphinx-rtd-tutorial.readthedocs.io/en/latest/sphinx-config.html#autodoc-configuration
+sys.path.insert(0, os.path.abspath(f"../{project}/"))  # Required for autosummary + autodoc
+
+extensions = [
+    "sphinx.ext.duration",  # Benchmarks Sphinx Build Times
+    "sphinx.ext.autodoc",  # Extracts Docstrings from Code
+    'sphinx.ext.autosummary',  # Collects Code to Summarize Automatically with Autodoc
+    "sphinx.ext.coverage",  # Measures Package Documentation Coverage
+    "sphinx.ext.napoleon",  # Supports Numpy and Google Style Docstring Formatting
+    # "sphinx.ext.linkcode",  # Add Links to each documented element to original code
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
