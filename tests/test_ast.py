266e6012d1390c2f81b6f4c5598bce1474e697c4
Spill-Tea <delrioj17@gmail.com>
2024-04-09 12:37:45 -0700

    Silly initial AST Tests.

---
+++ tests/test_ast.py
@@ -0,0 +1,18 @@
+"""
+    test_ast.py
+
+"""
+import pytest
+
+from FhY.lang.ast import base
+
+
+@pytest.mark.parametrize("name", [
+    "test", "nombre", "badHombre", "Example"
+])
+def test_base_node_name(name):
+    """Silly Test to confirm that the Given Class Name is Carried through Inheritance."""
+    # Dynamically Construct a Class with a given Name
+    obj = type(name, (base.ASTNode, ), {})
+    ret = obj.name()
+    assert ret == name, f"Names are Not Equivalent: Returned(`{ret}`) vs Given(`{name}`)"
