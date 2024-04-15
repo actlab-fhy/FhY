0b61ff582f0c8ba0ead165a874fa36d4262a498a
Spill-Tea <delrioj17@gmail.com>
2024-04-15 13:50:08 -0700

    Code linting.

--- tests/test_ast.py
+++ tests/test_ast.py
@@ -1,18 +1,19 @@
 """
-    test_ast.py
+test_ast.py
 
 """
+
 import pytest
 
 from fhy.lang.ast import base
 
 
-@pytest.mark.parametrize("name", [
-    "test", "nombre", "badHombre", "Example"
-])
+@pytest.mark.parametrize("name", ["test", "nombre", "badHombre", "Example"])
 def test_base_node_name(name):
     """Silly Test to confirm that the Given Class Name is Carried through Inheritance."""
     # Dynamically Construct a Class with a given Name
-    obj = type(name, (base.ASTNode, ), {})
+    obj = type(name, (base.ASTNode,), {})
     ret = obj.keyname()
-    assert ret == name, f"Names are Not Equivalent: Returned(`{ret}`) vs Given(`{name}`)"
+    assert (
+        ret == name
+    ), f"Names are Not Equivalent: Returned(`{ret}`) vs Given(`{name}`)"
