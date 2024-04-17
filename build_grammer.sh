0161a4506a71e148a256dceb7418525c51c825b7
Spill-Tea <delrioj17@gmail.com>
2024-04-17 12:23:13 -0700

    Setup Gihub Action on Remote Repository. Provide simple build script for local setup.

---
+++ build_grammer.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+JAVA_VERSION=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+ANTLR_VERSION=$(antlr4 | awk '/Version /{print $NF}')
+
+printf "Java Version $JAVA_VERSION\n"
+printf "Antlr Version $ANTLR_VERSION\n"
+
+printf "Building Files from FhY Grammar...\n\n"
+antlr4 -Dlanguage=Python3 grammar/FhY.g4
+
+printf "Finished Building Files from FhY Grammar!\n"
+
+printf "Now Moving Files into FhY Project\n"
+mv grammar/*.py fhy/lang/parser/
+printf "Finished Moving FhY Parser Files!\n"
