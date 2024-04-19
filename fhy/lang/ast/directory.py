6289d1fdbe7eb88ae2468889995ba4b258fd5ea7
Christopher Priebe <chrisp9090@gmail.com>
2024-04-19 13:44:48 -0700

    Changed list slice to more readable built-in call

--- fhy/lang/ast/directory.py
+++ fhy/lang/ast/directory.py
@@ -22,7 +22,7 @@ _ast_node_types: Dict[type[ASTNode], ASTNodeTypeInfo] = {}
 
 def get_ast_node_type_info(ast_node_class: type[ASTNode]) -> ASTNodeTypeInfo:
     """Collects Typing Information from an ASTNode that has been previously registered.
-    
+
     Raises:
         UnregisteredASTNode: When a class has not been previously registered.
 
@@ -38,7 +38,7 @@ def _get_ast_node_fields(ast_node_class: type[ASTNode]) -> Dict[str, type]:
     fields: Dict[str, type] = {}
     # NOTE: In the event a Subclass Overwrites an attribute, traverse the
     #       MRO Backwards, to prioritize the final Atribute Definition
-    for cls in ast_node_class.mro()[::-1]:
+    for cls in reversed(ast_node_class.mro()):
         if ASTNode in cls.mro():
             fields.update(cls.__annotations__)
     return fields
