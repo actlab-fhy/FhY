415fd8d7880aeb0252b238c11df9b89e7094dc63
Spill-Tea <delrioj17@gmail.com>
2024-04-29 13:24:56 -0700

    Use modified conftest fixtures.

--- tests/unit/test_symbol_table_builder.py
+++ tests/unit/test_symbol_table_builder.py
@@ -1,17 +1,11 @@
 import pytest
 
-import fhy.lang
-import fhy.lang.ast
 from fhy import ir
-from fhy.lang import ast
 from fhy.lang.passes import build_symbol_table
 from fhy.lang.passes.symbol_table_builder import (
     AlreadyDeclaredIdentifierException,
     UndeclaredIdentifierException,
 )
-from fhy.lang.printer import pprint_ast
-
-from ..utils import construct_ast, lexer, list_to_types, parser
 
 
 def _get_symbol_table_string_keys(
@@ -20,9 +14,13 @@ def _get_symbol_table_string_keys(
     return {symbol.name_hint for symbol in symbol_table.keys()}
 
 
-def test_empty_program(parser):
+def test_empty_program(construct_ast):
+    """Tests that an empty program builds a symbol table with one namespace, containing
+    no variables.
+
+    """
     source_file_content = ""
-    _ast = construct_ast(parser, source_file_content)
+    _ast = construct_ast(source_file_content)
 
     symbol_table = build_symbol_table(_ast)
 
@@ -31,9 +29,10 @@ def test_empty_program(parser):
     assert len(module_namespace) == 0
 
 
-def test_empty_procedure(parser):
+def test_empty_procedure(construct_ast):
+    """Tests Empty Procedure Body contains procedure name in symbol table."""
     source_file_content = "proc main() {}"
-    _ast = construct_ast(parser, source_file_content)
+    _ast = construct_ast(source_file_content)
 
     symbol_table = build_symbol_table(_ast)
 
@@ -47,9 +46,13 @@ def test_empty_procedure(parser):
     assert len(main_namespace_symbol_table) == 0
 
 
-def test_procedure_with_arguments(parser):
+def test_procedure_with_arguments(construct_ast):
+    """Tests Empty Procedure Argument names are in the symbol table procedure
+    namespace.
+
+    """
     source_file_content = "proc main(input int32[A, B] a, input int32[A, C] b) {}"
-    _ast = construct_ast(parser, source_file_content)
+    _ast = construct_ast(source_file_content)
 
     symbol_table = build_symbol_table(_ast)
 
@@ -61,16 +64,16 @@ def test_procedure_with_arguments(parser):
 
     main_namespace_symbol_table = list(symbol_table.values())[1]
     assert len(main_namespace_symbol_table) == 5
-    assert "a" in _get_symbol_table_string_keys(main_namespace_symbol_table)
-    assert "b" in _get_symbol_table_string_keys(main_namespace_symbol_table)
-    assert "A" in _get_symbol_table_string_keys(main_namespace_symbol_table)
-    assert "B" in _get_symbol_table_string_keys(main_namespace_symbol_table)
-    assert "C" in _get_symbol_table_string_keys(main_namespace_symbol_table)
+    for char in "abABC":
+        assert char in _get_symbol_table_string_keys(
+            main_namespace_symbol_table
+        ), f"Expected Variable in Symbol table: `{char}`"
 
 
-def test_procedure_with_declaration_statement(parser):
+def test_procedure_with_declaration_statement(construct_ast):
+    """Tests procedure body variables are in symbol table procedure namespace"""
     source_file_content = "proc main(input int32[A, B] a) {temp int32[A] b;}"
-    _ast = construct_ast(parser, source_file_content)
+    _ast = construct_ast(source_file_content)
 
     symbol_table = build_symbol_table(_ast)
 
@@ -82,41 +85,49 @@ def test_procedure_with_declaration_statement(parser):
 
     main_namespace_symbol_table = list(symbol_table.values())[1]
     assert len(main_namespace_symbol_table) == 4
-    assert "a" in _get_symbol_table_string_keys(main_namespace_symbol_table)
-    assert "b" in _get_symbol_table_string_keys(main_namespace_symbol_table)
-    assert "A" in _get_symbol_table_string_keys(main_namespace_symbol_table)
-    assert "B" in _get_symbol_table_string_keys(main_namespace_symbol_table)
+
+    for char in "abAB":
+        assert char in _get_symbol_table_string_keys(
+            main_namespace_symbol_table
+        ), f"Expected Variable in Symbol table: `{char}`"
 
 
-def test_fails_with_undefined_shape_variable(parser):
+def test_fails_with_undefined_shape_variable(construct_ast):
+    """Tests an error is raised with an undeclared shape variable, `C`."""
     source_file_content = "proc main(input int32[A, B] a) {temp int32[C] b;}"
-    _ast = construct_ast(parser, source_file_content)
+    _ast = construct_ast(source_file_content)
 
     with pytest.raises(UndeclaredIdentifierException):
         build_symbol_table(_ast)
 
 
-def test_fails_with_already_defined_variable(parser):
+def test_fails_with_already_defined_variable(construct_ast):
+    """Tests redefinition of a variable raises an error."""
     source_file_content = "proc main(input int32[A, B] a) {temp int32[A] a;}"
-    _ast = construct_ast(parser, source_file_content)
+    _ast = construct_ast(source_file_content)
 
     with pytest.raises(AlreadyDeclaredIdentifierException):
         build_symbol_table(_ast)
 
 
-def test_fails_with_already_defined_procedure(parser):
+def test_fails_with_already_defined_procedure(construct_ast):
+    """Tests that redefining a procedure name raises an error."""
     source_file_content = "proc main() {} proc main() {}"
-    _ast = construct_ast(parser, source_file_content)
+    _ast = construct_ast(source_file_content)
 
     with pytest.raises(AlreadyDeclaredIdentifierException):
         build_symbol_table(_ast)
 
 
-def test_import_variable(parser):
+def test_import_variable(construct_ast):
+    """Tests import of a variable, and it's usage within a procedure, is present in both
+    module level and procedure level namespaces.
+
+    """
     source_file_content = (
         "import constants.pi; proc main() {temp int32 a = constants.pi;}"
     )
-    _ast = construct_ast(parser, source_file_content)
+    _ast = construct_ast(source_file_content)
 
     symbol_table = build_symbol_table(_ast)
 
@@ -134,9 +145,10 @@ def test_import_variable(parser):
     assert "a" in _get_symbol_table_string_keys(main_namespace_symbol_table)
 
 
-def test_fails_with_already_defined_import(parser):
+def test_fails_with_already_defined_import(construct_ast):
+    """Tests that reimporting the same variable raises an error."""
     source_file_content = "import constants.pi; import constants.pi;"
-    _ast = construct_ast(parser, source_file_content)
+    _ast = construct_ast(source_file_content)
 
     with pytest.raises(AlreadyDeclaredIdentifierException):
         build_symbol_table(_ast)
