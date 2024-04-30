9de0ddd1790872d3832beb0d7ce2db57204da652
Spill-Tea <delrioj17@gmail.com>
2024-04-30 10:57:59 -0700

    additional coverage of pretty printing through integration tests. Specifically ternary expressions, selection statements, float literals, index types with stride, and declaration statements with assignment. Fix selection statement formatting of pretty printer.

--- fhy/lang/printer/printer.py
+++ fhy/lang/printer/printer.py
@@ -113,11 +113,10 @@ class ASTPrettyPrinter(BasePass):
         self._decrement_indent()
         condition = self.visit(selection_statement.condition)
 
-        text = f"if ({condition}) " + "{\n" + true_body + f"\n{self._spacer}" + "}"
+        text = f"if {condition} " + "{\n" + true_body + f"\n{self._spacer}" + "}"
 
-        # TODO: Confirm this Comes out right...
-        if false_body != "":
-            text += " else {\n" + true_body + f"\n{self._spacer}" + "}"
+        if len(false_body) > 0:
+            text += " else {\n" + false_body + f"\n{self._spacer}" + "}"
 
         return text
 
