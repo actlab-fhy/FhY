6b9215da559d289fc5386610bf721c7965777b1f
Christopher Priebe <chrisp9090@gmail.com>
2024-08-08 15:15:50 -0700

    fix(fhy.ir.table): Revamped the symbol table.

--- src/fhy/ir/table/__init__.py
+++ src/fhy/ir/table/__init__.py
@@ -31,7 +31,6 @@
 
 """The base table mapping and frame definitions comprise the core SymbolTable."""
 
-from .base import Table
 from .symbol_table import (
     FunctionSymbolTableFrame,
     ImportSymbolTableFrame,
