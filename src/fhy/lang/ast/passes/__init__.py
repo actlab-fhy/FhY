98a3d8af960778d7e0a71356c200e6bc46e695c1
Christopher Priebe <47607274+christopherpriebe@users.noreply.github.com>
2024-11-12 12:34:10 -0800

    Index Collector Pass (#8)
    * feat(lang.ast.passes.index_collector): Added index collector.
    
    * fix(index_collector): Updated Identifier to come from FhY Core.

--- src/fhy/lang/ast/passes/__init__.py
+++ src/fhy/lang/ast/passes/__init__.py
@@ -34,4 +34,5 @@
 from .identifier_collector import collect_identifiers
 from .identifier_replacer import replace_identifiers
 from .imported_identifier_collector import collect_imported_identifiers
+from .index_collector import collect_indices, collect_reduced_indices
 from .symbol_table_builder import build_symbol_table
